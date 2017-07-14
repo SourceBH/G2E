dbf3ca8b8ca6c8c8d36fe43d147e3f829b672c33
SourceBH <hackbloxer@gmail.com>
2017-07-14 19:23:33 -0400

    Add files via upload

---
+++ G2E.rb
@@ -0,0 +1,75 @@
+#This is created by Oci Sova/Source
+#On Brick-Hill, and Source is just learning
+#Ruby, so please report any problems to Source.
+#//Start Of Areas
+#//Area Zero
+puts("Welcome to G2E (GST TO Eastern Time)")
+sleep(2)
+#//Area One
+puts("Created by Oci Sova/Source")
+sleep(2)
+#//Area Two
+puts("")
+#//Area Three
+puts("Please note that if you enter anything other then a")
+puts("Number, then whatever you have entered will become a 0.")
+puts("Please do not take credit for this program!")
+#//Area Four
+puts("")
+#//Area Five
+sleep(3)
+#//Area Six
+puts("GST Example: 15-07-2017")
+sleep(1)
+#//Area Seven
+puts("Now, put in the first number")
+sleep(1)
+#//Area Eight
+puts("Example: FirstNumber-SecondNumber-ThirdNumber")
+FirstNumber = gets.chop.to_i
+sleep(1.5)
+#//Area Nine
+puts("Now put in the second number!")
+SecondNumber = gets.chop.to_i
+sleep(1.5)
+#//Area Ten
+puts("And finally, put in the third number.")
+ThirdNumber = gets.chop.to_i
+sleep(2)
+#//Area Eleven
+print("Converting")
+sleep(1.5)
+print(".")
+sleep(1.5)
+print(".")
+sleep(1.5)
+print(".")
+sleep(1.5)
+print(".")
+sleep(1.5)
+puts(".")
+sleep(0.5)
+#//Area Twelve
+puts("The GST time has been converted to:")
+sleep(2.0)
+#//Area Thirteen
+print("#{SecondNumber}-#{FirstNumber}-#{ThirdNumber}")
+puts(" | Eastern Time!")
+sleep(1.5)
+#//Area Fourteen
+puts("Eastern Format: MONTH-DAY-YEAR")
+sleep(3.0)
+puts("")
+#//Area Fifteen
+puts("Did you like my program? If so, you can follow me at")
+puts("https://twitter.com/SourceBH to show your support and love for me!")
+puts("Suggest what I should create next time by messaging me on Brick-Hill.")
+#//Quick Sleep Area #1
+Sleep(4)
+#//Area Sixteen
+puts("https://www.brick-hill.com/messages/compose?id=3610")
+#//Area Seventeen
+print("You should copy the Eastern Time, this will close in #{time}")
+Time = "17 Seconds"
+sleep(17)
+#//End of Areas
---
+++ README.txt
@@ -0,0 +1,10 @@
+1. Go download ruby at https://rubyinstaller.org/downloads/
+2. Open G2E
+
+CREDIT GOES TO OCI SOVA/SOURCE.
+This is pretty basic, I know, but I
+wanted to contribute to the community
+by doing something!
+
+Follow me at twitter.com/SourceBH
+Send me a message at https://www.brick-hill.com/messages/compose?id=3610
